fabed47b2b13429288ab27cd1b334ae03a25c00e
root <root@blobstation.localdomain>
2023-04-15 23:30:10 +0530

    this is auto commit

--- ...spread_operator.js
+++ ...spread_operator.js
@@ -1,7 +1,25 @@
+// SPREAD OPERATOR
+
 const user = ['tom','holland','perry','jection','northon']
 const userid= ['ew#1','e2@w','ew%2']
-
-
 const grabInOne = ['rao','shyam',...user,...userid]
 
-console.log({grabInOne})
+//SPREAD ARGS TO FUNCTION
+
+const multiplier=(multiplierBy,...args)=>{
+    return args.map((value)=>{
+        return multiplierBy * value
+    })
+}
+
+
+const mutiplyValue = (multiplier(2,1,2,6))
+
+
+const userAGE = {
+    name: '',
+    age: null,
+  };
+  
+  const age = userAGE.age ?? 11;
+  console.log(age); // Output: 0
